c415de498c46cdcb5fb441694b37541bcf32bed8
DESKTOP-RR8873I\Lenovo <k.akchurina@of-group.ru>
2023-12-12 21:01:41 +0300

    add kafka extensions

---
+++ back-end/src/FileFormatter.Messaging/Extensions/MessagingExtensions.cs
@@ -0,0 +1,21 @@
+﻿using FileFormatter.Messaging.Abstractions;
+using FileFormatter.Messaging.Options;
+using FileFormatter.Messaging.Producers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FileFormatter.Messaging.Extensions;
+
+public static class MessagingExtensions
+{
+    public static IServiceCollection AddKafkaMessaging(this IServiceCollection services)
+    {
+        services.AddOptions().ConfigureOptions<KafkaSettings>();
+
+        services
+            .AddScoped<IFileReadyToDownloadProducer, FileReadyToDownloadProducer>()
+            .AddScoped<INewRequestProducer, NewRequestProducer>()
+            .AddScoped<IProcessingResultProducer, ProcessingResultProducer>();
+
+        return services;
+    }
+}
