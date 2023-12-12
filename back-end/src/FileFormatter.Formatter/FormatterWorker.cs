c17a966d9725cecffb5718c922295b737e5c2616
DESKTOP-RR8873I\Lenovo <k.akchurina@of-group.ru>
2023-12-12 21:11:41 +0300

    add formatter worker

---
+++ back-end/src/FileFormatter.Formatter/FormatterWorker.cs
@@ -0,0 +1,44 @@
+﻿using Confluent.Kafka;
+using FileFormatter.Formatter.Abstractions;
+using FileFormatter.Messaging.Abstractions;
+using FileFormatter.Messaging.Common;
+using FileFormatter.Messaging.Dto;
+using FileFormatter.Messaging.Options;
+using Microsoft.Extensions.Options;
+
+namespace FileFormatter.Formatter;
+
+public class FormatterWorker : IListener
+{
+    private readonly ISingleFileProcessor _singleFileProcessor;
+    private readonly IConsumer<Guid, EnqueTaskMessage> _consumer;
+
+    public FormatterWorker(ISingleFileProcessor singleFileProcessor, IOptions<KafkaSettings> settings)
+    {
+        _singleFileProcessor = singleFileProcessor;
+
+        var config = settings.Value.BuildConsumerConfiguration();
+
+        _consumer = new ConsumerBuilder<Guid, EnqueTaskMessage>(config).Build();
+        _consumer.Subscribe(settings.Value.NewRequestTopic);
+    }
+
+    public Task Run(CancellationToken cancellationToken)
+    {
+        Task.Run(async () =>
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                var next = _consumer.Consume(cancellationToken);
+                if (next != null)
+                {
+                    await _singleFileProcessor.ProcessNextAsync(
+                        next.Message.Value.RequestId, 
+                        next.Message.Value.FileName);
+                    _consumer.Commit();
+                }
+            }
+        });
+        return Task.CompletedTask;
+    }
+}
