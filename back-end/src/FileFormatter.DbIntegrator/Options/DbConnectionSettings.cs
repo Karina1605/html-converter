224809c065782427e73a15d556a0d3da19a0e020
DESKTOP-RR8873I\Lenovo <k.akchurina@of-group.ru>
2023-12-12 20:57:46 +0300

    add db connection configuration

---
+++ back-end/src/FileFormatter.DbIntegrator/Options/DbConnectionSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace FileFormatter.DbIntegrator.Options;
+
+public class DbConnectionSettings
+{
+    public const string SectionName = nameof(DbConnectionSettings);
+
+    public string ConnecitonString { get; set; }
+
+    public string TableName { get; set; }
+}
