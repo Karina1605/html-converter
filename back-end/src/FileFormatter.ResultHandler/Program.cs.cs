c50c730ac0f5b6065422047fb9376091b0287bf6
DESKTOP-RR8873I\Lenovo <k.akchurina@of-group.ru>
2023-12-12 21:13:20 +0300

    add service checking results of processing, keeps db up to date

---
+++ back-end/src/FileFormatter.ResultHandler/Program.cs.cs
@@ -0,0 +1,27 @@
+﻿using FileFormatter.DbIntegrator.Extensions;
+using FileFormatter.LinksService.Extensions;
+using FileFormatter.Messaging.Abstractions;
+using FileFormatter.Messaging.Extensions;
+using FileFormatter.ResultHandler;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var hostBuilder = Host.CreateDefaultBuilder()
+    .ConfigureServices(services =>
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile($"appsettings.json");
+
+        var config = configuration.Build();
+
+        services.AddDbServices(config);
+        services.AddKafkaMessaging();
+        services.AddLinkBuilderClient(config);
+        services.AddSingleton<IListener, ResultsListener>();
+
+    }).Build();
+hostBuilder.Start();
+var listener = hostBuilder.Services.GetRequiredService<IListener>();
+await listener.Run(CancellationToken.None);
---
+++ back-end/src/FileFormatter.ResultHandler/ResultsListener.cs
@@ -0,0 +1,78 @@
+﻿using Confluent.Kafka;
+using FileFormatter.DbIntegrator.Abstractions.Query;
+using FileFormatter.LinksService.Constracts;
+using FileFormatter.Messaging.Abstractions;
+using FileFormatter.Messaging.Common;
+using FileFormatter.Messaging.Dto;
+using FileFormatter.Messaging.Options;
+using Microsoft.Extensions.Options;
+
+namespace FileFormatter.ResultHandler;
+
+public class ResultsListener : IListener
+{
+    private readonly IConsumer<Guid, TaskProcessingResult> _listener;
+
+    private readonly IFileReadyToDownloadProducer _producer;
+    private readonly ILinkBuilderClient _linkBuilderClient;
+    private readonly IUpdateStateQuery _updateStatequery;
+
+    public ResultsListener(
+        IFileReadyToDownloadProducer producer,
+        ILinkBuilderClient linkBuilderClient,
+        IOptions<KafkaSettings> options,
+        IUpdateStateQuery updateStateQuery)
+    {
+        _producer = producer;
+        _linkBuilderClient = linkBuilderClient;
+        _updateStatequery = updateStateQuery;
+
+        var config = options.Value.BuildConsumerConfiguration();
+
+        _listener = new ConsumerBuilder<Guid, TaskProcessingResult>(config).Build();
+        _listener.Subscribe(options.Value.FileProcessedTopic);
+    }
+
+    public Task Run(CancellationToken cancellationToken)
+    {
+        Task.Run(async () =>
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                var next = _listener.Consume(cancellationToken);
+                if (next != null)
+                {
+                    await _updateStatequery.UpdateFileState(
+                        next.Message.Key,
+                        next.Message.Value.OriginalFileName,
+                        next.Message.Value.Result,
+                        next.Message.Value.NewFileName,
+                        cancellationToken);
+
+                    if (next.Message.Value.Result == Common.Enums.FileProcessingStatus.Converted)
+                    {
+                        var link = await _linkBuilderClient.GenerateLinkAsync(new GenerateLinkRequest(
+                            next.Message.Key,
+                            next.Message.Value.NewFileName!));
+                        await _producer.OnReadyToDownloadAsync(new ProcessingFinalResult(
+                            next.Message.Key, 
+                            next.Message.Value.OriginalFileName,
+                            next.Message.Value.Result, 
+                            link));
+                    }
+
+                    else if (next.Message.Value.Result == Common.Enums.FileProcessingStatus.ProcessingFailed)
+                    {
+                        await _producer.OnReadyToDownloadAsync(new ProcessingFinalResult(
+                          next.Message.Key,
+                          next.Message.Value.OriginalFileName,
+                          next.Message.Value.Result,
+                          null));
+                    }
+                }
+            }
+
+        });
+        return Task.CompletedTask;
+    }
+}
